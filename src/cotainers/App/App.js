2bfee2d25cd1ec9a3c228080b807a2a3d446b2cf
伍文 <wuwen@wuwendeMBP-2.princeton.epam.com>
2019-03-20 14:18:15 +0800

    Add tabBar

--- src/cotainers/App/App.js
+++ src/cotainers/App/App.js
@@ -4,7 +4,8 @@
  */
 
 import React from 'react';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { Provider } from 'react-redux';
 
 import SearchUsers from '../SearchUsers/SearchUsers';
@@ -13,26 +14,44 @@ import About from '../About/About';
 import Contacts from '../Contacts/Contacts';
 import store from '../../store/store';
 
-const Navigator = createStackNavigator(
+const TabNavigator = createBottomTabNavigator(
   {
-    Home: {
-      screen: SearchUsers
-    },
-    Trends: {
-      screen: Trends
-    },
-    About: {
-      screen: About
-    },
-    Contacts: {
-      screen: Contacts
-    }
+    Home: SearchUsers,
+    Trends: Trends,
+    About: About,
+    Contacts: Contacts,
   },
   {
-    initialRouteName: 'Trends'
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Home':
+          iconName = 'home';
+            break;
+          case 'Trends':
+            iconName = 'chart-line';
+            break;
+          case 'About':
+            iconName = 'sun';
+            break;
+          case 'Contacts':
+            iconName = 'headset';
+            break;
+          default:
+            break;
+        }
+        return <FontAwesome5 name={iconName} size={20} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#76cdd8',
+      inactiveTintColor: '#888',
+    },
   }
 );
-const AppContainer = createAppContainer(Navigator);
+const AppContainer = createAppContainer(TabNavigator);
 
 export default class App extends React.Component {
   render () {
